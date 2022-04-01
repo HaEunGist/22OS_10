790fd531dea6f9c6d3c1bb6324702f6233edc53d
12novel30 <79260472+12novel30@users.noreply.github.com>
2022-04-01 14:28:11 +0900

    v0.2.2
    - ASSERT 삭제

--- project2/thread_edit.c
+++ project2/thread_edit.c
@@ -36,11 +36,12 @@ thread_yield (void)  // thread.c
 
   old_level = intr_disable ();
   if (cur != idle_thread) 
-    list_insert_ordered (&ready_list, &t->elem, compare, NULL);	/*haeun*/
+    list_insert_ordered (&ready_list, &cur->elem, compare, NULL);	/*haeun*/
   cur->status = THREAD_READY;
   schedule ();
   intr_set_level (old_level);
 }
+
 
 void
 thread_unblock (struct thread *t) // thread.c //simultaneus
@@ -52,8 +53,8 @@ thread_unblock (struct thread *t) // thread.c //simultaneus
   ASSERT (t->status == THREAD_BLOCKED); //original
 
   struct thread *cur = running_thread(); //from schedule
-  ASSERT (cur->status == THREAD_RUNNING); //from schedule
-  if compare(&t->elem, &cur->elem, NULL){ //steal running!
+  //ASSERT (cur->status == THREAD_RUNNING); //from schedule
+  if (compare(&t->elem, &cur->elem, NULL)){ //steal running!
       t->status = THREAD_RUNNING; //like init_thread
       list_push_front(&ready_list, &cur->elem); //like thread_yield
       cur->status = THREAD_READY; //like thread_yield
@@ -61,7 +62,7 @@ thread_unblock (struct thread *t) // thread.c //simultaneus
   }
   else //~steal
   {
-    list_insert_ordered (&ready_list, &cur->elem, compare, NULL); //original
+    list_insert_ordered (&ready_list, &t->elem, compare, NULL); //original
     t->status = THREAD_READY; //original
   }
 
