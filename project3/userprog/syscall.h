7cdba9f372ad3795985c33aa207842eb03d2b6c6
12novel30 <79260472+12novel30@users.noreply.github.com>
2022-05-15 23:53:07 +0900

    v0.0.3
    final

--- project3/userprog/syscall.h
+++ project3/userprog/syscall.h
@@ -20,17 +20,3 @@ unsigned tell (int fd);
 void close (int fd);
 
 #endif /* userprog/syscall.h */
-
-
-
-
-/*이 아래가 원본 - 위에꺼는 안됨 오류남*/
-#ifndef USERPROG_SYSCALL_H
-#define USERPROG_SYSCALL_H
-
-void syscall_init (void);
-
-
-#endif /* userprog/syscall.h */
-
-
