7e9cc9c67ec5c467db428651fbd244f43e3fb155
HaEunGist <101621673+HaEunGist@users.noreply.github.com>
2022-05-20 22:00:32 +0900

    ADD: check_str
    pg 322

--- userprog/syscall.c
+++ userprog/syscall.c
@@ -255,11 +255,21 @@ struct vm_entry *check_add_in_vme(void* add, void* esp){
 /* Buffer의 주소가 유효한 가상주소인지 검사 */
 void check_buffer (void* buffer, unsigned size, void* esp){
   void *ptr = pg_round_down(buffer);
-  for (; ptr< buffer+size; ptr += PGSIZE){    //COPY, NEED FIX
+  for (; ptr< buffer + size; ptr += PGSIZE){    //COPY, NEED FIX
     if (check_add_in_vme(ptr, esp) == NULL){    //struct * vme = check_add_in_vme(ptr, esp); 였는데 * 필요할 수도 있음..
       exit(-1);
     }
     if (check_add_in_vme(ptr, esp)->writable == false){
+      exit(-1);
+    }
+  }
+}
+
+/* 인자의 string의 주소가 유효한 가상주소인지 검사 */
+void check_str (const void *str, unsigned len, void *esp){
+  void *ptr = pg_round_down(str);
+  for (; ptr< str + len; ptr += PGSIZE){    //COPY, NEED FIX
+    if (check_add_in_vme(ptr, esp) == NULL){    //struct * vme = check_add_in_vme(ptr, esp); 였는데 * 필요할 수도 있음..
       exit(-1);
     }
   }
