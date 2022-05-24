d71b66d78c43ce1d5bf12bca8580b789c5b3ee9b
HaEunGist <101621673+HaEunGist@users.noreply.github.com>
2022-05-24 18:03:06 +0900

    From 20205149

--- vm/page.h
+++ vm/page.h
@@ -1,12 +1,19 @@
-#define VM_BIN
-#define VM_FILE
-#define VM_ANON
+
+/*====proj4====*/
+
+//#ifndef VM_PAGE_H
+//#define VM_PAGE_H
+
+#define VM_BIN  0
+#define VM_FILE 1
+#define VM_ANON 2
+
+#include <hash.h>
 
 struct vm_entry {
     uint8_t type;
     void *vaddr;
     bool writable;   
-
     bool is_loaded;  
     struct file* file;
 
@@ -20,14 +27,22 @@ struct vm_entry {
 
     struct hash_elem elem;
 
-}
+};
+
+struct mmap_file
+{
+    int mapid;
+    struct file* file;
+    struct list_elem elem;
+    struct list vme_list;
+};
 
 void vm_init (struct hash *vm);
 static unsigned vm_hash_func (const struct hash_elem *e,void *aux);
 static bool vm_less_func (const struct hash_elem *a, const struct hash_elem *b);
-
 bool insert_vme (struct hash *vm, struct vm_entry *vme);
 bool delete_vme (struct hash *vm, struct vm_entry *vme);
-
 struct vm_entry *find_vme (void *vaddr);
 void vm_destroy (struct hash *vm);
+static void vm_destroy_func(struct hash_elem *e, void *aux);
+bool load_file (void *kaddr, struct vm_entry *vme);
